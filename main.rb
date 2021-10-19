10391791d400625af54d49834ebc9ff94fd8d5c1
himanshurd <91275023+himanshurd@users.noreply.github.com>
2021-10-19 12:43:12 -0700

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,11 +1,11 @@
 class Student
   MAX_COURSES = 5
 
-  def remove_from_schedule(course, schedule, quarter_id)
-    quarter[quarter_id].remove(course)
+  def remove_from_schedule(course_list)
+    course_list.remove(course)
   end
 
-  def add_to_schedule(course, schedule, quarter_id)
+  def add_to_schedule(course_list)
     max_courses = course_list.maximum_number_of_courses
     if max_courses < MAX_COURSES
       course_list.add(course)
